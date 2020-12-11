68260a8369d8600e457297c9f8518e4dbd1eadc9
antpngl92 <nyagolov.anton@gmail.com>
2020-12-11 10:27:18 +0000

    structure

--- allPOS/settings.py
+++ allPOS/settings.py
@@ -20,8 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('secret_key.txt') as f:
-    SECRET_KEY = f.read().strip()
+# with open('secret_key.txt') as f:
+#     SECRET_KEY = f.read().strip()
+SECRET_KEY = ')$sm!(c4*azsm1#1!&458%0(ybzn26v+z*j!)owece_=i0y=#u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
